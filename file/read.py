4bcdc4a87dc1bad0955625002797cd01e8f71a58
MaeStrive <1741733558@qq.com>
2024-03-24 16:34:51 +0800

    菌株管了

--- file/read.py
+++ file/read.py
@@ -1,7 +1,23 @@
 import pandas as pd
-df = pd.read_csv("data2.csv",header=None,encoding='gbk')
-data = df.values.tolist()
-# 将数据写入 JavaScript 文件
-with open('jidata2.js', 'w', encoding='utf-8') as jsfile:
-    jsfile.write("var jidata2 = " + str(data))
-print(data)
+
+# 读取CSV文件
+data = pd.read_csv('data.csv',encoding='gbk')
+
+# 获取标题行
+header = data[:2]
+
+# 获取数据行
+data = data[2:]
+
+# 将数据拆分成多个数组
+split_data = [data[i:i+61] for i in range(0, len(data), 61)]
+
+# 将数据写入多个JavaScript文件
+num_files = 10
+for i in range(num_files):
+    with open(f'junzhudata{i+1}.js', 'w',encoding='gbk') as file:
+        file.write('const data = [\n')
+        for _, row in split_data[i].iterrows():
+            row_values = [f'"{val}"' if isinstance(val, str) else str(val) for val in row]
+            file.write(f'[{", ".join(row_values)}],\n')
+        file.write('];')
